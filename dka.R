24d047988a3d36d0bfb02888f749c6a1c300b3e1
Y.Y <3901776+arkbeer@users.noreply.github.com>
2018-10-05 12:21:40 +0900

    Update dka.R

--- dka.R
+++ dka.R
@@ -1,5 +1,5 @@
 f<-function(x){
-  return(x^2+2*x+1)
+  return(x^3+2*x^2+4*x^1+1)
 }
 d<-function(f,x){
   h<-10^(-6)
@@ -18,6 +18,7 @@ dka<-function(f,a1,n,r0){
   for(i in 1:n){
     zk.0<-c(zk.0,a1/n+r0*exp(1i*(2*(i-1)*pi/n+pi/2/n)))
   }
+  prod(zk.0[i]-zk.0[-i])
   
   return(zk.0)
 }
