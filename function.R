f23bde126ffecf9d19281d4bcbeec5bdd2e70724
Y.Y <3901776+arkbeer@users.noreply.github.com>
2018-10-25 18:06:26 +0900

    Update function.R

--- function.R
+++ function.R
@@ -2,5 +2,5 @@ is_odd<-function(n){
   return(n%%2!=0)
 }
 atoi<-function(str){
-  return(str %>% chartr("a-z","A-Z",.) %>% str_split("") %>% unlist() %>% match(c(0:9,LETTERS)) %>% -1 %>% str_flatten())
+  return(str %>% chartr("a-z","A-Z",.) %>% str_split("") %>% map(~{match(.,c(0:9,LETTERS)) %>% -1 %>% str_flatten()}) %>% unlist())
 }
