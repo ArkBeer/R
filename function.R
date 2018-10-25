67eaf88647a39c1a7df5c73321518828e5f7482f
Y.Y <3901776+arkbeer@users.noreply.github.com>
2018-10-25 17:30:25 +0900

    Create function.R

---
+++ function.R
@@ -0,0 +1,6 @@
+is_odd<-function(n){
+  return(n%%2!=0)
+}
+atoi<-function(str){
+  return(str %>% chartr("a-z","A-Z",.) %>% str_split("") %>% unlist() %>% match(c(0:9,LETTERS)) %>% -1 %>% str_flatten())
+}
